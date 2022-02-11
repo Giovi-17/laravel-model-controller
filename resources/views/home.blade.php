c344a8847152063000ae7bc651b7c5ce1e05459c
Giovi-17 <giovanni.nuzzo17@gmail.com>
2022-02-11 15:40:49 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>Title: [title]</h2>
+    <span>Original Title: [original_title] - </span>
+    <span>Nationality: [nationality] - </span>
+    <span>Date: [date] - </span>
+    <span>Vote: [vote]</span>
+    
+</body>
+</html>
