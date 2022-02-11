db8dfaaf4345ca17c887fcebd8bfd1d46b1c218f
Giovi-17 <giovanni.nuzzo17@gmail.com>
2022-02-11 16:46:01 +0100

    BONUS(suddivisione delle card nella sezione dedicata)

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+                    
+    <h2><span class="bold">Title:</span> {{ $movie->title }}</h2>
+    <span><span class="bold">Original Title:</span> {{ $movie->original_title }} - </span>
+    <span><span class="bold">Nationality:</span> {{ $movie->nationality }} - </span>
+    <span><span class="bold">Date:</span> {{ $movie->date }} - </span>
+    <span><span class="bold">Vote:</span> {{ $movie->vote }}</span>
+
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
+@section('title')
+Homepage
+@endsection
+
+@section('main_content')
 
     <div class="container">
 
         @foreach ($movies as $movie)
-            
-            <div class="card">
                 
-                <h2><span class="bold">Title:</span> {{ $movie->title }}</h2>
-                <span><span class="bold">Original Title:</span> {{ $movie->original_title }} - </span>
-                <span><span class="bold">Nationality:</span> {{ $movie->nationality }} - </span>
-                <span><span class="bold">Date:</span> {{ $movie->date }} - </span>
-                <span><span class="bold">Vote:</span> {{ $movie->vote }}</span>
-
-            </div>
+            @include('components/card')
 
         @endforeach
 
     </div>
-    
-</body>
-</html>
+
+@endsection
